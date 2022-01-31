139de9a5744863c7969a31380ab9b19a13ba8fec
Cristian Valadez <c_valadez@outlook.com>
2022-01-31 16:36:26 -0600

    Implement all functions related to following centerline except for controls. * Only computes the distance error based on current position, does not implement optional future distance error yet * Does not move the vehicle yet, only displays error. Useful for testing since you can still move car with keyboard

--- wall_following/scripts/sae_wall_following.py
+++ wall_following/scripts/sae_wall_following.py
@@ -11,102 +11,109 @@ from ackermann_msgs.msg import AckermannDriveStamped
 import pdb
 
 # Vehicle parameters
-ANGLE_RANGE = 270 				# Hokuyo 10LX has 270 degrees scan
-DISTANCE_RIGHT_THRESHOLD = 0.5 	# (m)
-VELOCITY = 0.1 					# meters per second
+ANGLE_RANGE = 270  # Hokuyo 10LX has 270 degrees scan
+DISTANCE_RIGHT_THRESHOLD = 0.5  # (m)
+VELOCITY = 0.1  # meters per second
 
 # Controller parameters
 kp = 0.7
-kd = 
+# kd =
 
 # Other global variables
+COS_SIN_45 = np.cos(np.pi / 4)  # cos(45deg) = sin(45deg)
 error = 0.0
 prev_error = 0.0
 
+
 def control(error):
-	global kp
-	global kd
-	global VELOCITY
+    global kp
+    # global kd
+    global VELOCITY
 
-	# TO-DO: Implement controller
-	# ---
+    # TO-DO: Implement controller
+    # ---
+    steering_angle = 0  # just here to remove red squiggles
 
-	# ---
+    # Set maximum thresholds for steering angles
+    if steering_angle > 0.5:
+        steering_angle = 0.5
+    elif steering_angle < -0.5:
+        steering_angle = -0.5
 
-	# Set maximum thresholds for steering angles
-	if steering_angle > 0.5:
-		steering_angle = 0.5
-	elif steering_angle < -0.5:
-		steering_angle = -0.5
+    print("Steering Angle is = %f" % steering_angle)
 
-	print "Steering Angle is = %f" % steering_angle 
 
-	# TO-DO: Publish the message
-	# ---
+# TO-DO: Publish the message
 
-	# ---
 
 def get_index(angle, data):
-	# 	# TO-DO: For a given angle, return the corresponding index for the data.ranges array
-	# ---
-
-	# ---
-
-def distance(angle_right, angle_lookahead, data):
-	global ANGLE_RANGE
-	global DISTANCE_RIGHT_THRESHOLD
-
-	# TO-DO: Find index of the two rays, and calculate a, b, alpha and theta. Find the actual distance from the right wall.
-	# ---
-
-	# ---
-
-	print "Distance from right wall : %f" % distance_r
-
-	# Calculate error
-	error = 
-
-	return error, distance
+    # For a given angle, return the corresponding index for the data.ranges array
+    index = int(np.round((np.deg2rad(angle - 90) - data.angle_min) / data.angle_increment + 1, 0))
+    return index
 
 
-def follow_center(angle_right,angle_lookahead_right, data):
+def distance(angle, angle_lookahead, data):
+    global ANGLE_RANGE
+    global DISTANCE_RIGHT_THRESHOLD
+    global COS_SIN_45
 
-	angle_lookahead_left = 180 + angle_right
-	angle_left = 180 - angle_lookahead_right 
+    # Find the actual distance from the wall.
+    # alpha is the angle of the vehicle from the desired path
+    # alpha = tan^-1( (a*cos(theta) - b) / (a*sin(theta)))
+    # a = distance at angle_lookahead
+    # b = distance at angle_right
+    # theta = angle between angle_right and angle_lookahead, which was chosen as 45 deg
+    # Taking advantage that cos(45 deg) = sin(45 deg)
+    # alpha = tan^-1(1 + b/(a*sin(theta)))
 
-	er, dr = distance(angle_right, angle_lookahead_right, data)
-	el, dl = distance(angle_left, angle_lookahead_left, data)
+    a = data.ranges[get_index(angle, data)]
+    b = data.ranges[get_index(angle_lookahead, data)]
+    alpha = np.arctan(1 + b / (a * COS_SIN_45))
+    dist_m = b * np.cos(alpha)
 
-	# Find Centerline error
-	# ---
+    # For greater accuracy, calculate predicted error
+    # dist_p = dist_m + L*np.sin(alpha)
 
-	# ---
+    # Calculate error
+    error = DISTANCE_RIGHT_THRESHOLD - dist_m
 
-	print "Centerline error = %f " % centerline_error
+    return error, dist_m
 
-	return centerline_error
+
+def follow_center(angle_right, angle_lookahead_right, data):
+    angle_lookahead_left = 180 + angle_right
+    angle_left = 180 - angle_lookahead_right
+
+    er, dr = distance(angle_right, angle_lookahead_right, data)
+    el, dl = distance(angle_left, angle_lookahead_left, data)
+
+    # Find Centerline error
+    centerline_error = er - el
+    print("Distance from left wall : %f" % dl)
+    print("Distance from right wall : %f" % dr)
+    print("Centerline error = %f " % centerline_error)
+
+    return centerline_error
+
 
 def callback(data):
+    # Pick two rays at two angles
+    angle_right = 0
+    angle_lookahead = 45
 
-	# Pick two rays at two angles
-	angle_right = 
-	angle_lookahead = 
+    # To follow right wall
+    # er, dr = distance(angle_right,angle_lookahead, data)
 
-	# To follow right wall
-	#er, dr = distance(angle_right,angle_lookahead, data)
+    # To follow the centerline
+    ec = follow_center(angle_right, angle_lookahead, data)
 
-	# To follow the centerline
-	ec = follow_center(angle_right,angle_lookahead, data)
 
-	control(ec)
+# control(ec)
+
 
 if __name__ == '__main__':
-	print("Wall following started")
-	rospy.init_node('wall_following',anonymous = True)
+    print("Wall following started")
+    rospy.init_node('wall_following', anonymous=True)
+    s = rospy.Subscriber("/scan", LaserScan, callback)
 
-	# TO-DO: Implement the publishers and subscribers
-	# ---
-
-	# ---
-
-	rospy.spin()
+    rospy.spin()
