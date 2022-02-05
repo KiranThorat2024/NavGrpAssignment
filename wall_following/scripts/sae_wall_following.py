508228c8e8ac7b39ea195091489755ec8a98a11e
Vivek Bonangi <vivek.bonangi@navistar.com>
2022-02-04 21:27:25 -0600

    Committing changes to the branch

--- wall_following/scripts/sae_wall_following.py
+++ wall_following/scripts/sae_wall_following.py
@@ -11,102 +11,177 @@ from ackermann_msgs.msg import AckermannDriveStamped
 import pdb
 
 # Vehicle parameters
-ANGLE_RANGE = 270 				# Hokuyo 10LX has 270 degrees scan
-DISTANCE_RIGHT_THRESHOLD = 0.5 	# (m)
-VELOCITY = 0.1 					# meters per second
+ANGLE_RANGE = 270  # Hokuyo 10LX has 270 degrees scan
+DISTANCE_RIGHT_THRESHOLD = 0.5  # (m)
+VELOCITY = 1.35  # meters per second
 
-# Controller parameters
-kp = 0.7
-kd = 
+# PID Control parameters
+kp = 2
+kd = 0.0075
+error_prior = 0.0
+steering_angle_prior = 0.0
+kpv = 10  # P controller for velocity
 
 # Other global variables
-error = 0.0
-prev_error = 0.0
+error_prior = 0.0
+dl_pr = 0.0
+dr_pj_pr = 0.0
+dr_pr = 0.0
+cl_err_pr = 0.0
+rw_err_pr = 0.0
+sa_pr = 0.0
+vel_pr = 0.0
+al_pr = 0.0
+RIGHT_WALL_MODE = 1  # 0=centerline, 1=right wall
+
+pub = rospy.Publisher('/vesc/ackermann_cmd_mux/input/teleop', AckermannDriveStamped, queue_size=1)
+
 
 def control(error):
-	global kp
-	global kd
-	global VELOCITY
+    global kp, kd
+    global kpv
+    global VELOCITY
+    global error_prior, steering_angle_prior
+    global sa_pr, vel_pr
 
-	# TO-DO: Implement controller
-	# ---
+    # Steering PD controller
+    derivative = (error - error_prior)
+    steering_angle = kp * error + kd * derivative
 
-	# ---
+    # Velocity P controller
+    steering_delta = steering_angle - steering_angle_prior
+    velocity = VELOCITY - kpv * steering_delta
 
-	# Set maximum thresholds for steering angles
-	if steering_angle > 0.5:
-		steering_angle = 0.5
-	elif steering_angle < -0.5:
-		steering_angle = -0.5
+    # Update priors
+    error_prior = error
+    steering_angle_prior = steering_angle
 
-	print "Steering Angle is = %f" % steering_angle 
+    # Set maximum thresholds for steering angles
+    if steering_angle > 0.5:
+        steering_angle = 0.5
+    elif steering_angle < -0.5:
+        steering_angle = -0.5
 
-	# TO-DO: Publish the message
-	# ---
+    # Try to slow down on sharp turns
+    if steering_angle > 0.45:
+        velocity = 0.15
+    elif steering_angle > 0.4:
+        velocity = 0.25
+    elif steering_angle > 0.325:
+        velocity = 0.6
+    elif steering_angle > 0.25:
+        velocity = 0.9
 
-	# ---
+    # Set maximum threshold for velocity
+    if velocity > VELOCITY:
+        velocity = VELOCITY
+
+    # Store for debugging
+    sa_pr = "%.3f" % steering_angle
+    vel_pr = "%.3f" % velocity
+
+    # Publish the message
+    msg = AckermannDriveStamped()
+    msg.drive.speed = velocity
+    msg.drive.steering_angle = steering_angle
+    pub.publish(msg)
+
 
 def get_index(angle, data):
-	# 	# TO-DO: For a given angle, return the corresponding index for the data.ranges array
-	# ---
-
-	# ---
-
-def distance(angle_right, angle_lookahead, data):
-	global ANGLE_RANGE
-	global DISTANCE_RIGHT_THRESHOLD
-
-	# TO-DO: Find index of the two rays, and calculate a, b, alpha and theta. Find the actual distance from the right wall.
-	# ---
-
-	# ---
-
-	print "Distance from right wall : %f" % distance_r
-
-	# Calculate error
-	error = 
-
-	return error, distance
+    # For a given angle, return the corresponding index for the data.ranges array
+    index = int(np.round((np.deg2rad(angle - 90) - data.angle_min) / data.angle_increment + 1, 0))
+    return index
 
 
-def follow_center(angle_right,angle_lookahead_right, data):
+def distance(angle, angle_lookahead, data):
+    # Find the actual distance from the wall.
+    # alpha is the angle of the vehicle from the desired path
+    # alpha = tan^-1( (a*cos(theta) - b) / (a*sin(theta)))
+    # a = distance at angle_lookahead
+    # b = distance at angle_right
+    # theta = angle between angle_right and angle_lookahead
+    theta = np.deg2rad(np.abs(angle_lookahead - angle))
+    a = data.ranges[get_index(angle_lookahead, data)]
+    b = data.ranges[get_index(angle, data)]
+    alpha = np.arctan((a * np.cos(theta) - b) / (a * np.sin(theta)))
+    dist_m = b * np.cos(alpha)
+    dist_p = dist_m + 0.375 * np.sin(alpha)
 
-	angle_lookahead_left = 180 + angle_right
-	angle_left = 180 - angle_lookahead_right 
+    return dist_m, dist_p, alpha
 
-	er, dr = distance(angle_right, angle_lookahead_right, data)
-	el, dl = distance(angle_left, angle_lookahead_left, data)
 
-	# Find Centerline error
-	# ---
+def follow_center(angle_right, angle_lookahead_right, data):
+    global dl_pr, dr_pr, cl_err_pr
 
-	# ---
+    angle_lookahead_left = 180 - angle_lookahead_right
+    angle_left = 180 - angle_right
 
-	print "Centerline error = %f " % centerline_error
+    dr = distance(angle_right, angle_lookahead_right, data)
+    dl = distance(angle_left, angle_lookahead_left, data)
 
-	return centerline_error
+    # Find Centerline error
+    centerline_error = dr - dl
+
+    # Store for debugging
+    dl_pr = "%.3f" % dl
+    dr_pr = "%.3f" % dr
+    cl_err_pr = "%.3f" % centerline_error
+    return centerline_error
+
+
+def follow_right_wall(angle_right, angle_lookahead, data):
+    global DISTANCE_RIGHT_THRESHOLD
+    global dr_pr, rw_err_pr, dr_pj_pr, al_pr
+
+    dr, dr_p, alpha = distance(angle_right, angle_lookahead, data)
+
+    # Calculate error to right wall
+    error = DISTANCE_RIGHT_THRESHOLD - dr_p
+
+    # Store for debugging
+    dr_pr = "%.3f" % dr
+    dr_pj_pr = "%.3f" % dr_p
+    rw_err_pr = "%.3f" % error
+    al_pr = "%.3f" % np.rad2deg(alpha)
+
+    return error
+
 
 def callback(data):
+    global dl_pr, dr_pr, dr_pj_pr, rw_err_pr, cl_err_pr, sa_pr, vel_pr, al_pr
 
-	# Pick two rays at two angles
-	angle_right = 
-	angle_lookahead = 
+    # Pick two rays at two angles
+    angle_right = 0
+    angle_lookahead = 30
 
-	# To follow right wall
-	#er, dr = distance(angle_right,angle_lookahead, data)
+    if RIGHT_WALL_MODE:
+        # To follow right wall
+        er = follow_right_wall(angle_right, angle_lookahead, data)
+        control(er)
+        # For debugging, print variables
+        sys.stdout.write('\r' + "{:<7} {:<7} {:<7} {:<7} {:<7} {:<7}".
+                         format(dr_pr, dr_pj_pr, rw_err_pr, sa_pr, vel_pr, al_pr))
+    else:
+        # To follow the centerline
+        ec = follow_center(angle_right, angle_lookahead, data)
+        control(ec)
+        # For debugging, print variables
+        sys.stdout.write('\r' + "{:<9} {:<9} {:<11} {:<7} {:<7}".
+                         format(dl_pr, dr_pr, cl_err_pr, sa_pr, vel_pr))
 
-	# To follow the centerline
-	ec = follow_center(angle_right,angle_lookahead, data)
-
-	control(ec)
 
 if __name__ == '__main__':
-	print("Wall following started")
-	rospy.init_node('wall_following',anonymous = True)
+    print("Wall following started")
+    rospy.init_node('wall_following', anonymous=True)
 
-	# TO-DO: Implement the publishers and subscribers
-	# ---
+    if RIGHT_WALL_MODE:
+        print("{:<7} {:<7} {:<7} {:<7} {:<7} {:<7}".
+              format('Right', 'Proj', 'Err', 'Steer', 'Vel', 'Alpha'))
+    else:
+        print("{:<9} {:<9} {:<11} {:<7} {:<11}".
+              format('Left', 'Right', 'C-line Err', 'Steer', 'Vel'))
 
-	# ---
+    # Start the subscriber
+    rospy.Subscriber("/scan", LaserScan, callback)
+    rospy.spin()
 
-	rospy.spin()
---
+++ wall_following/scripts/sae_wall_following_Combined.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python3
+
+import rospy
+import numpy as np
+import sys
+from sensor_msgs.msg import LaserScan
+from ackermann_msgs.msg import AckermannDriveStamped
+
+# Vehicle parameters
+ANGLE_RANGE = 270  # Hokuyo 10LX has 270 degrees scan
+DISTANCE_RIGHT_THRESHOLD = 0.5  # (m)
+VELOCITY_C = 1.35  # Maximum meters per second
+VELOCITY_R = 1.8   # Maximum meters per second
+# PID Control parameters defaults for centerline wall follow
+kp = 1.5
+kd = 0.005
+ki = 0.75
+
+# Other global variables
+steering_angle_prior = 0.0
+error_prior = 0.0
+integral_prior = 0.0
+dl_pr = 0.0
+dr_pj_pr = 0.0
+dr_pr = 0.0
+cl_err_pr = 0.0
+rw_err_pr = 0.0
+sa_pr = 0.0
+sa_delta_pr = 0.0
+vel_pr = 0.0
+al_pr = 0.0
+RIGHT_WALL_MODE = 1  # 0=centerline, 1=right wall
+rate = 40.0
+rate_s = 1.0 / rate
+velocity = 0.0
+data_buffer = LaserScan()
+angle_right = 0         # Pick two rays at two angles
+angle_lookahead = 30    # lookahead angle
+
+pub = rospy.Publisher('/vesc/ackermann_cmd_mux/input/teleop', AckermannDriveStamped, queue_size=1)
+
+
+def control(error):
+    global kp, kd, ki
+    global velocity, VELOCITY_C, VELOCITY_R
+    global error_prior, integral_prior, steering_angle_prior
+    global sa_pr, vel_pr, sa_delta_pr
+
+    # Steering PID controller
+    derivative = (error - error_prior)/rate_s
+    integral = integral_prior + error*rate_s
+    steering_angle = kp*error + kd*derivative + ki*integral
+    #sa_pr = "%.3f" % steering_angle
+    # Update priors
+    error_prior = error
+    integral_prior = integral
+    
+    if RIGHT_WALL_MODE:
+    # Set maximum thresholds for steering angles
+      if steering_angle > 0.75:
+         steering_angle = 0.75
+      elif steering_angle < -0.45:
+           steering_angle = -0.45
+    else:
+        if steering_angle > 0.5:
+           steering_angle = 0.5
+        elif steering_angle < -0.5:
+           steering_angle = -0.5
+        
+        #steering_angle = steering_angle
+                
+    steering_delta = steering_angle - steering_angle_prior
+    steering_angle_prior = steering_angle
+       
+    if RIGHT_WALL_MODE:
+      if error > 0.14:
+         velocity = 0.35
+      elif error < 0.14:   
+           velocity = VELOCITY_R - kp*error
+      elif error < -0.7:  
+           velocity = 0.35   
+      elif error < 0.01:  
+           velocity = VELOCITY_R + kp*error   
+      else:
+           velocity = VELOCITY_R
+         
+    else:
+      if np.abs(error) < 0.1:
+         velocity = VELOCITY_C
+      elif np.abs(error) < 0.125:
+           velocity = 1.25
+      elif np.abs(error) < 0.15:
+           velocity = 1.0
+      elif np.abs(error) < 0.16:
+           velocity = 0.75
+      else:
+           velocity = VELOCITY_C
+    # Set maximum threshold for velocity
+    #if velocity > VELOCITY_C:
+      # velocity = VELOCITY_C
+    # Set maximum threshold for velocity
+    if velocity > VELOCITY_R:
+       velocity = VELOCITY_R   
+    # Store for debugging
+    sa_pr = "%.3f" % steering_angle
+    vel_pr = "%.3f" % velocity
+    sa_delta_pr = "%.3f" % steering_delta
+
+    # Publish the message
+    msg = AckermannDriveStamped()
+    msg.drive.speed = velocity
+    msg.drive.steering_angle = steering_angle
+    pub.publish(msg)
+
+
+def get_index(angle, data):
+    # For a given angle, return the corresponding index for the data.ranges array
+    index = int(np.round((np.deg2rad(angle - 90) - data.angle_min) / data.angle_increment, 0))
+    return index
+
+
+def distance(angle, angle_la, data):
+    global velocity
+    # Find the actual distance from the wall.
+    # alpha is the angle of the vehicle from the desired path
+    # alpha = tan^-1( (a*cos(theta) - b) / (a*sin(theta)))
+    # a = distance at angle_lookahead
+    # b = distance at angle_right
+    # theta = angle between angle_right and angle_lookahead
+    theta = np.deg2rad(np.abs(angle_la - angle))
+    a = data.ranges[get_index(angle_la, data)]
+    b = data.ranges[get_index(angle, data)]
+
+    # Exit if distance cannot be determined
+    if np.isnan(a) or np.isnan(b):
+        return np.nan, np.nan, np.nan
+
+    alpha = np.arctan((a * np.cos(theta) - b) / (a * np.sin(theta)))
+    dist_m = b * np.cos(alpha)
+    dist_p = dist_m + 0.85 * np.sin(alpha)
+
+    return dist_m, dist_p, alpha
+
+
+def follow_center(ray_angle_right, ray_angle_right_lookahead, data):
+    global dl_pr, dr_pr, cl_err_pr, al_pr, error_prior
+
+    ray_angle_left_lookahead = 180 - ray_angle_right_lookahead
+    ray_angle_left = 180 - ray_angle_right
+
+    dr, dr_p, right_alpha = distance(ray_angle_right, ray_angle_right_lookahead, data)
+    dl, dl_p, alpha = distance(ray_angle_left, ray_angle_left_lookahead, data)
+
+    # Find Centerline error
+    if np.isnan(dr_p) or np.isnan(dl_p):
+        centerline_error = error_prior
+    else:
+        centerline_error = dl_p - dr_p
+
+    # Store for debugging
+    dl_pr = "%.3f" % dl_p
+    dr_pr = "%.3f" % dr_p
+    cl_err_pr = "%.3f" % centerline_error
+    al_pr = "%.1f" % right_alpha
+    return centerline_error
+
+
+def follow_right_wall(angle, angle_la, data):
+    global DISTANCE_RIGHT_THRESHOLD
+    global dr_pr, rw_err_pr, dr_pj_pr, al_pr
+
+    dr, dr_p, alpha = distance(angle, angle_la, data)
+
+    # Calculate error to right wall
+    if np.isnan(dr_p):
+        error = error_prior
+    else:
+        error = DISTANCE_RIGHT_THRESHOLD - dr_p
+
+    # Store for debugging
+    dr_pr = "%.3f" % dr
+    dr_pj_pr = "%.3f" % dr_p
+    rw_err_pr = "%.3f" % error
+    al_pr = "%.1f" % np.rad2deg(alpha)
+
+    return error
+
+
+def callback(data):
+    global data_buffer
+    data_buffer = data  # Store the data into the buffer
+
+
+def run():
+    global angle_right, angle_lookahead, data_buffer, RIGHT_WALL_MODE
+    global dl_pr, dr_pr, dr_pj_pr, rw_err_pr, kp, kd, ki
+    global cl_err_pr, sa_pr, vel_pr, al_pr, sa_delta_pr
+
+    # Run loop at steady rate until Ctr-C or program is terminated
+    while not rospy.is_shutdown():
+        if RIGHT_WALL_MODE:
+            # Override the PID gains for the right wall follow mode
+            kp = 2.35
+            kd = 0.09
+            ki = 0.15
+
+            # To follow right wall
+            er = follow_right_wall(angle_right, angle_lookahead, data_buffer)
+            control(er)
+
+            # For debugging, print variables
+            sys.stdout.write("{:<7} {:<7} {:<7} {:<7} {:<7} {:<7} {:<7}\n".
+                             format(dr_pr, dr_pj_pr, rw_err_pr, sa_pr, vel_pr, al_pr, sa_delta_pr))
+        else:
+            # To follow the centerline
+            ec = follow_center(angle_right, angle_lookahead, data_buffer)
+            control(ec)
+
+            # For debugging, print variables
+            sys.stdout.write("{:<7} {:<7} {:<7} {:<7} {:<7} {:<7} {:<7}\n".
+                             format(dl_pr, dr_pr, cl_err_pr, sa_pr, vel_pr, al_pr, sa_delta_pr))
+
+        r.sleep()
+
+
+if __name__ == '__main__':
+    # Get command line argument if it was given, else defaults to Right Wall follow mode
+    if len(sys.argv) == 2:
+        RIGHT_WALL_MODE = int(sys.argv[1])
+
+    rospy.init_node('wall_following', anonymous=True)
+    r = rospy.Rate(rate)
+
+    if RIGHT_WALL_MODE:
+
+        print("Wall follow 0.5 from right wall")
+        print("{:<7} {:<7} {:<7} {:<7} {:<7} {:<7} {:<7}".
+              format('Right', 'Proj', 'Err', 'Steer', 'Vel', 'Alpha', 'SA Delta'))
+    else:
+        print("Wall follow using centerline")
+        print("{:<7} {:<7} {:<7} {:<7} {:<7} {:<7} {:<7}".
+              format('Left', 'Right', 'Err', 'Steer', 'Vel', 'Alpha', 'SA Delta'))
+
+    rospy.Subscriber("/scan", LaserScan, callback)  # Start the subscriber
+
+    # Wait for subscriber to receive at least first message
+    while len(data_buffer.ranges) == 0:
+        r.sleep()
+
+    # Subscriber got data at least once, start program
+    run()
+
