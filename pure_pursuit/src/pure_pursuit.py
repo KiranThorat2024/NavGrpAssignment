15c9da853ef496ec186fbcb3bf888cb3b2885de1
Cristian Valadez <c_valadez@outlook.com>
2022-03-11 01:02:41 -0600

    Add some more to the controller

--- pure_pursuit/src/pure_pursuit.py
+++ pure_pursuit/src/pure_pursuit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # Imports
+from ackermann_msgs.msg import AckermannDriveStamped
 import rospy
 from race.msg import drive_param
 from geometry_msgs.msg import PoseStamped
@@ -26,19 +27,20 @@ class PurePursuit(object):
         # Subscribers, publishers, topics, etc
         rospy.Subscriber("/vesc/odom", Odometry, self.pose_callback)
         self.pub = rospy.Publisher("drive_parameters", drive_param, queue_size=1)  # velocity and angle
+        self.pub2 = rospy.Publisher('/vesc/ackermann_cmd_mux/input/teleop', AckermannDriveStamped, queue_size=1)
         self.rate = rospy.Rate(_rate)
 
-        # Waypoints, car variables
+        # Waypoints, car variables, constants, etc.
         self.waypoints = _waypoints
         self.waypoints_x = []
         self.waypoints_y = []
         for point in self.waypoints:
             self.waypoints_x.append(float(point[0]))
             self.waypoints_y.append(float(point[1]))
-        # for i in range(len(way_x)):
-        #     print("i: {:d} x: {:.3f} y: {:.3f}".format(i, way_x[i], way_y[i]))
         self.LOOKAHEAD = 1.2
         self.WB = 0.3421
+        self.MAX_VELOCITY = 0.6
+        self.k = 0.75
 
         # Current robot position object
         self.pose = self.CurrPose()
@@ -77,7 +79,7 @@ class PurePursuit(object):
         return distance
 
     def interpolate(self, _idx):
-        # 2 Line points (x1, y1) and (x2, y2)
+        # 2 points (x1, y1) and (x2, y2) to form line
         x1 = float(self.waypoints[_idx][0])
         y1 = float(self.waypoints[_idx][1])
         x2 = float(self.waypoints[_idx + 1][0])
@@ -91,7 +93,7 @@ class PurePursuit(object):
         # Interpolation algorithm
         # Find intersection between circle of radius LOOKAHEAD and two nearest waypoints
         # TODO: NEED TO UPDATE THIS VERTICAL LINE SECTION
-        if x2 == x1:  # Need to check for vertical line to avoid slope calc divide by 0
+        if x2 == x1:  # Need to check for vertical line to avoid slope calculation divide by 0
             # Calculate distance from point (center of circle to line)
             dist = np.abs(h - x1)
 
@@ -191,14 +193,71 @@ class PurePursuit(object):
                 # Find nearest waypoint and calculated next waypoint lookahead distance away
                 nearest_idx = self.find_nearest_waypoint()
                 idx_near_lookahead = self.idx_close_to_lookahead(nearest_idx)
-                print("\nCurrent pose: ({:.2f}, {:.2f})".format(self.pose.x, self.pose.y))
-                print("Nearest Idx: {:d} Near LA Idx: {:d}".format(nearest_idx, idx_near_lookahead))
+                print("\nCurrent pose: ({:.2f}, {:.2f} Nearest Idx: {:d} Near LA Idx: {:d})".
+                      format(self.pose.x, self.pose.y, nearest_idx, idx_near_lookahead))
 
                 # Calculate target pose using interpolation
                 target_x, target_y = self.interpolate(idx_near_lookahead)
 
+                # Show some animations
                 if self.show_animation:
                     self.animate(target_x, target_y, idx_near_lookahead)
+
+                # Calculate error = alpha = theta - yaw
+                # theta is slope of line between reference and waypoint
+                # yaw is orientation of vehicle from odometer
+                rise = target_y - self.pose.y
+                run = target_x - self.pose.x
+
+                theta = np.arctan(np.abs(rise / run))
+                if run > 0:  # Going forward
+                    if rise > 0:  # Positive slope
+                        e = theta - self.pose.yaw
+                    elif rise < 0:  # Negative slope
+                        e = -theta - self.pose.yaw
+                    else:  # Horizontal line pointing right
+                        e = - self.pose.yaw
+                elif run < 0:  # Going backward
+                    if rise > 0:  # Negative slope
+                        e = np.pi - theta - self.pose.yaw
+                    elif rise < 0:  # Positive slope
+                        e = np.pi + theta - self.pose.yaw
+                    else:  # Horizontal line pointing left
+                        e = np.pi - self.pose.yaw
+                else:  # Vertical line
+                    if rise > 0:  # Pointing up
+                        e = np.pi / 2 - self.pose.yaw
+                    elif rise < 0:  # Pointing down
+                        e = 3 * np.pi / 2 - self.pose.yaw
+                    else:  # On the dot, don't know what to do...
+                        print("Error: Current pose and waypoint are equal")
+                        break
+
+                # A different method for calculating e
+                # ld = eucl_dist(self.pose.x, self.pose.y, target_x, target_y)
+                # e = ld * np.sin(self.pose.yaw)
+
+                # P controller for steering angle
+                steering_angle = e * self.k
+                if steering_angle > 0.5:
+                    steering_angle = 0.5
+                if steering_angle < -0.5:
+                    steering_angle = -0.5
+                print("Theta: {:.2f} Yaw: {:.2f} Error: {:.2f} Steering angle: {:.2f}".
+                      format(theta, self.pose.yaw, e, steering_angle))
+
+                # For now just use constant velocity
+                velocity = self.MAX_VELOCITY
+
+                # Publish the message
+                # msg = drive_param()
+                # msg.velocity = velocity
+                # msg.angle = steering_angle
+                # self.pub.publish(msg)
+                msg = AckermannDriveStamped()
+                msg.drive.speed = velocity
+                msg.drive.steering_angle = steering_angle
+                self.pub2.publish(msg)
 
                 self.rate.sleep()
         except IndexError:
@@ -266,6 +325,6 @@ if __name__ == '__main__':
     rospy.init_node('pure_pursuit', anonymous=True)
 
     # Create object and pass the waypoints to it
-    pure_pursuit = PurePursuit(_waypoints=read_points(), _rate=1)
+    pure_pursuit = PurePursuit(_waypoints=read_points(), _rate=5)
     print("Attempting to follow waypoints...")
     pure_pursuit.follow_waypoints()
