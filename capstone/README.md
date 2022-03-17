a4689c588c8056e6ba9b7fca70b6a69b8661ed80
Cristian Valadez <c_valadez@outlook.com>
2022-03-17 09:42:08 -0500

    Add capstone project folder

---
+++ capstone/README.md
@@ -0,0 +1,11 @@
+# Capstone Project #
+Group 4 - Navistar
+
+* Cristian Valadez
+* Vivek Bonangi
+* Amy Kofoed
+* Kiran Thorat
+
+Task1: (B) Obstacle avoidance with follow the gap
+Task2: (A) Line following with object detection
+Task3: (A) Go-to-goal using the Navigation Stack
---
+++ capstone/src/capstone.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Imports
+import rospy
+
+
+class Capstone(object):
+    def __init__(self):
+        # Subscribers, publishers, topics, etc
+
+        # Variables, constants, etc.
+        pass
+
+
+if __name__ == '__main__':
+    rospy.init_node('capstone', anonymous=True)
+    capstone = Capstone()
