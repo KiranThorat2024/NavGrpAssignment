4fd3dc9e3af9552756fe876e1d54b7d9f0e3d6b8
Cristian Valadez <c_valadez@outlook.com>
2022-03-18 17:52:53 -0500

    Implement algorithm to find largest gap for task 1

--- capstone/src/capstone.py
+++ capstone/src/capstone.py
@@ -12,6 +12,7 @@ class Capstone(object):
 
         # Subscribers, publishers, topics, etc
         self.ls_sub = rospy.Subscriber("/scan", LaserScan, self.ls_callback)  # Laser scan subscriber
+        self.rate = rospy.Rate(1)
 
         # Wait until we get at least one data back from callbacks
         while self.laserScanData is None:
@@ -22,16 +23,30 @@ class Capstone(object):
     def ls_callback(self, _data):
         self.laserScanData = _data
 
-    def task1(self):
+    def find_largest_gap(self):
         # See image in README file on how ranges are defined
-        ranges = [np.array(self.laserScanData.ranges[81:135]),
-                  np.array(self.laserScanData.ranges[28:82]),
-                  np.array(self.laserScanData.ranges[0:28] + self.laserScanData.ranges[334:360]),
-                  np.array(self.laserScanData.ranges[281:335]),
-                  np.array(self.laserScanData.ranges[228:282])]
+        ranges = np.array([self.laserScanData.ranges[228:282],
+                           self.laserScanData.ranges[281:335],
+                           self.laserScanData.ranges[0:28] + self.laserScanData.ranges[334:360],
+                           self.laserScanData.ranges[28:82],
+                           self.laserScanData.ranges[81:135]])
 
+        # Max range of laser scan sensor is 3.5m. Here we replace 'infinite' with 5m so that we can compare
+        # accurately when multiple ranges have infinite values
+        ranges[ranges == np.inf] = 5.0
         ranges_ss = ss_iterable(ranges)
-        print("ranges sum of squares: ".format(ranges_ss))
+        return np.argmax(ranges_ss)+1
+
+    def task1(self):
+        while True:
+            gap = self.find_largest_gap()
+            print("Largest gap at {}".format(gap))
+            # inf_array = ""
+            # for i in range(360):
+            #     if not self.laserScanData.ranges[i] == np.inf:
+            #         inf_array += str(i) + ", "
+            # print("\n Found something at: {}".format(inf_array))
+            self.rate.sleep()
 
 
 # An iterable sum of squares method
