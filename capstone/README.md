0d70b90b76138d31bcaeef06757d10dd317f4a42
Cristian Valadez <c_valadez@outlook.com>
2022-03-18 01:14:28 -0500

    Start development of gap follow algorithm

--- capstone/README.md
+++ capstone/README.md
@@ -1,4 +1,4 @@
-# Capstone Project #
+# Capstone Project
 Group 4 - Navistar
 
 * Cristian Valadez
@@ -6,6 +6,10 @@ Group 4 - Navistar
 * Amy Kofoed
 * Kiran Thorat
 
-* Task1: (B) Obstacle avoidance with follow the gap
-* Task2: (A) Line following with object detection
-* Task3: (A) Go-to-goal using the Navigation Stack
+# Tasks
+## Task1: (B) Obstacle avoidance with follow the gap
+The laser scan data was split up into 5 equal sized regions as shown in this image:
+![LaserScan ranges](FollowGap_LaserScanRanges.png)
+
+## Task2: (A) Line following with object detection
+## Task3: (A) Go-to-goal using the Navigation Stack
--- capstone/src/capstone.py
+++ capstone/src/capstone.py
@@ -1,16 +1,53 @@
 #!/usr/bin/env python3
-# Imports
+import time
 import rospy
+from sensor_msgs.msg import LaserScan
+import numpy as np
 
 
 class Capstone(object):
     def __init__(self):
-        # Subscribers, publishers, topics, etc
-
         # Variables, constants, etc.
-        pass
+        self.laserScanData = None
+
+        # Subscribers, publishers, topics, etc
+        self.ls_sub = rospy.Subscriber("/scan", LaserScan, self.ls_callback)  # Laser scan subscriber
+
+        # Wait until we get at least one data back from callbacks
+        while self.laserScanData is None:
+            print("Waiting for callbacks to get at least on data back")
+            time.sleep(1)
+
+    # LaserScan callback
+    def ls_callback(self, _data):
+        self.laserScanData = _data
+
+    def task1(self):
+        # See image in README file on how ranges are defined
+        ranges = [np.array(self.laserScanData.ranges[81:135]),
+                  np.array(self.laserScanData.ranges[28:82]),
+                  np.array(self.laserScanData.ranges[0:28] + self.laserScanData.ranges[334:360]),
+                  np.array(self.laserScanData.ranges[281:335]),
+                  np.array(self.laserScanData.ranges[228:282])]
+
+        ranges_ss = ss_iterable(ranges)
+        print("ranges sum of squares: ".format(ranges_ss))
+
+
+# An iterable sum of squares method
+def ss_iterable(_m):
+    _result = []
+    for i in range(len(_m)):
+        _ss = 0
+        for j in range(len(_m[i])):
+            _ss += _m[i][j] ** 2
+        _result.append(_ss)
+    return _result
 
 
 if __name__ == '__main__':
     rospy.init_node('capstone', anonymous=True)
     capstone = Capstone()
+
+    # Run Task 1
+    capstone.task1()
